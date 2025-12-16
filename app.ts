1999c8f95bcef5b1304c88e51e3726d2f2b4eca6
Vuong_Xuan_Khai <xuankhai19102004@gmail.com>
2025-12-16 14:40:50 +0700

    backend-25

---
+++ app.ts
@@ -0,0 +1,17 @@
+// const express = require("express");
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/bar", (req, res) => {
+  res.send("Hello, again!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
